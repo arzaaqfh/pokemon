f3aa77434fc0bcdb70effff8f4f76dc1d1c71076
arzaaqfh123 <arzaaq.falaahan.ali@student.upi.edu>
2020-12-25 10:33:01 +0700

    deploy my react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,290 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import { getData } from './Pokemon';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [Pokemon, setPokemon] = useState( [] );
+  const [NextList, setNextList] = useState( '' );
+  const [PrevList, setPrevList] = useState( '' );
+  const [Loading, setLoading] = useState(true);
+  const Url = 'https://pokeapi.co/api/v2/pokemon';
+  const [PokemonName, setPokemonName] = useState( '' );
+  const PokNickname = useRef ( null );
+  const [MyPokemon, setMyPokemon] = useState ( [{name: '', nickname: ''}] );
+  const [PokemonDetail, setPokemonDetail] = useState( [{name: '', nickname: '', img: '', abilities: [], moves: [], height: '', weight: '', types: []}] );
+  const [ShowForm, setShowForm] = useState(false);
+  
+  useEffect(() => {
+    const dataPokemonName = localStorage.getItem('pokemon-name');
+    const dataPokemonDetail = localStorage.getItem('pokemon-detail');
+    const dataMyPokemon = localStorage.getItem('my-pokemon-data');
+
+    if(dataPokemonName){
+      setPokemonName(dataPokemonName);
+    }
+    if(dataPokemonDetail){
+      setPokemonDetail(JSON.parse(dataPokemonDetail));
+    }
+    if(dataMyPokemon){
+      setMyPokemon(JSON.parse(dataMyPokemon));
+    }
+  }, [] );
+
+  useEffect(() => {
+    localStorage.setItem('pokemon-detail', JSON.stringify(PokemonDetail));
+    localStorage.setItem('my-pokemon-data', JSON.stringify(MyPokemon));
+    localStorage.setItem('pokemon-name', PokemonName);
+  } );
+
+  useEffect(() =>  {
+    async function fetchData() {
+      let data = await getData(Url);
+      setNextList(data.next);
+      setPrevList(data.previous);
+      setPokemon(data.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, [] );
+  
+  useEffect(() => {
+    if(PokemonName !== ''){
+      let name = PokemonName.split(';');
+      let data = getData(Url+'/'+name[0]);
+      
+      data.then(val => 
+        setPokemonDetail({
+          name: name[0],
+          nickname: name[1],
+          img: val.sprites.front_shiny,
+          abilities: val.abilities,
+          moves: val.moves,
+          height: val.height,
+          weight: val.weight,
+          types: val.types
+        })
+      )
+    }
+  }, [PokemonName]);
+
+  const next = async () => {
+    if(!NextList) return;
+    let data = await getData(NextList);
+    setPokemon(data.results);
+    setNextList(data.next);
+    setPrevList(data.previous);
+  }
+
+  const prev = async () => {
+    if(!PrevList) return;
+    let data = await getData(PrevList);
+    setPokemon(data.results);
+    setNextList(data.next);
+    setPrevList(data.previous);
+  }
+
+  function catchPokemon() {
+    var catchPok = Boolean(Math.round(0 + Math.random() * (1 - 0)));
+    setShowForm(catchPok);
+    return(
+      <>
+      { catchPok ? alert('Catch pokemon SUCCESS') : alert('Catch pokemon FAILED')} 
+      </>
+    )  
+  }
+
+  const addMyPokemon = () => {
+    let cek = MyPokemon.filter(val => val.nickname === PokNickname.current.value).length;
+    
+    if(cek === 0){
+      setShowForm(false);
+      setMyPokemon([ ... MyPokemon, {
+        name: PokemonDetail.name,
+        nickname: PokNickname.current.value
+      }])
+      PokNickname.current.value = '';
+      alert('The pokemon is added!');
+    }else{
+      setShowForm(true);
+      alert('Nickname is already exist!');
+    }
+
+  }
+
+  const releasePokemon = (data) => {
+    let newData  = MyPokemon.filter(val => { return val.nickname != data.nickname});
+    setMyPokemon(newData);    
+  }
+
+  //POKEMON PAGE
+  function pokemonPage() {
+    return(
+      <div className="row">
+        <label><h1>Pokemon List</h1></label>
+        <div className='btn-group'>
+          {PrevList ? (<button onClick={prev}>Prev</button>) : (<></>) }
+          {NextList ? (<button onClick={next}>Next</button>) : (<></>) }
+        </div>
+        <table className="tbl">
+          <thead>
+              <tr>
+                <th>Name</th>
+                <th>Total Owned</th>
+                <th>Action</th>
+              </tr>
+          </thead>
+            {Pokemon.map((pok, i) =>
+              <tr key={pok.name}>
+                <td>{pok.name}</td>
+                <td>{MyPokemon.filter(val => val.name === pok.name).length}</td>
+                <td><Link to={'/pokemon/'+pok.name+';'}>Detail</Link></td>
+              </tr>
+            )}
+        </table>
+      </div>
+    );
+  }
+
+  //POKEMON DETAIL PAGE
+  function showDetail( {match} ) {
+    setPokemonName(match.params.name);
+    return(
+      <>
+      { Loading ? <h1>LOADING...</h1> :(
+        <>
+        {ShowForm ? (
+          <>
+          <h1>Give The Pokemon Nickname</h1>
+          <form onSubmit={addMyPokemon}>
+            <label>Pokemon Nickname: </label>
+            <input name="nickname" type="text" ref={PokNickname} required/><br/>
+            
+            <button type='submit' className="btn-submit">Add</button>
+          </form>
+          </>
+        ) : null}
+
+        <label><h1>Pokemon Detail</h1></label>
+        <div className='detailBox'>
+          <div className='imageBox'>
+            <img src={PokemonDetail.img} />
+          </div>
+          <table>
+          {PokemonDetail.nickname !== '' ? (
+            <tr>
+              <td>Nickname</td>
+              <td>:</td>
+              <td>{PokemonDetail.nickname}</td>
+            </tr>
+          ):(<></>)}
+            <tr>
+              <td>Name</td>
+              <td>:</td>
+              <td>{PokemonDetail.name}</td>
+            </tr>
+            <tr>
+              <td>Move</td>
+              <td>:</td>
+              <td>
+                <ul>
+                  {PokemonDetail.moves.map(val=><li>{val.move.name}</li>)}
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>Abilities</td>
+              <td>:</td>
+              <td>
+                <ul>
+                  {PokemonDetail.abilities.map(val=><li>{val.ability.name}</li>)}
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>Type</td>
+              <td>:</td>
+              <td>
+                <ul>
+                  {PokemonDetail.types.map(val=><li>{val.type.name}</li>)}
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>Weight</td>
+              <td>:</td>
+              <td>{PokemonDetail.weight}</td>
+            </tr>
+            <tr>
+              <td>Height</td>
+              <td>:</td>
+              <td>{PokemonDetail.height}</td>
+            </tr>
+          </table>
+          {PokemonDetail.nickname === '' ? (
+            <button onClick={catchPokemon}>Catch the Pokemon</button>
+          ):(<></>)}          
+        </div>
+        </>
+      )}        
+      </>  
+    );
+  }
+
+  //MY POKEMON PAGE
+  function myPokemonPage() {
+    return(
+      <>
+      <div className="row">
+        <label><h1>My Pokemon List</h1></label>
+        <table className="tbl">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Nickname</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {MyPokemon.map(pok => 
+              <tr key={pok.id}>
+                <td>{pok.name}</td>
+                <td>{pok.nickname}</td>
+                <td>
+                  <Link to={'/pokemon/'+pok.name+';'+pok.nickname}>Detail</Link>
+                  <Link to={'/mypokemon'} className='btn-remove' onClick={releasePokemon.bind(this, pok)} >Release</Link>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      </>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    { Loading ? <h1>LOADING...</h1> :(
+      <div>
+        {/* Navigation Bar */}
+        <nav>
+          <ul>
+            <li><Link to='/'>Pokemon</Link></li>
+            <li><Link to='/mypokemon'>My Pokemon</Link></li>
+          </ul>
+        </nav>
+
+        <main>
+          <Switch>
+            <Route path='/' exact component={pokemonPage} />
+            <Route path='/pokemon/:name' exact component={showDetail} />
+            <Route path='/mypokemon' exact component={myPokemonPage} />
+          </Switch>
+        </main>
+      </div>
+    )}
+    </BrowserRouter>
   );
 }
 
